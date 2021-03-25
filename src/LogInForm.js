94d3b2e7f4bb597b106dba40d92c9cc54d4cecee
Dasha Lary <dasha4988@gmail.com>
2021-03-25 13:25:34 -0400

    finished submit function in loginform

--- src/LogInForm.js
+++ src/LogInForm.js
@@ -14,12 +14,41 @@ function LogInForm(props) {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        
+        fetch(`http://localhost:3000/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            localStorage.setItem("token", data.jwt)
+            props.handleLogin(data.user)
+        })
+        setUsername("")
+        setPassword("")
+
+    }
+
+    const formStyle = {
+        margin: "auto",
+        padding: "20px",
+        width: "60%"
     }
 
     return (
         <div>
+            <div style={formStyle}>
             <h2>Log In</h2>
+            <form onSubmit={handleSubmit}>
+                
+            </form>
+            </div>
         </div>
     )
 
